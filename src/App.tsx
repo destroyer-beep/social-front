2055c624531590ce882ab79085787c6349f5f6c9
Oleg <oleg-kozlov91@rambler.ru>
2024-04-27 15:31:00 +0300

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!
         </a>
       </header>
     </div>
